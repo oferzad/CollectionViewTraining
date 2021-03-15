11ec917ed31f4b7dc3bc5dde2b4b811d663904f7
Oferzad <ofer.zadikario@gmail.com>
2021-03-15 12:08:10 +0200

    first push

---
+++ CollectionViewTraining/CollectionViewTraining/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CollectionViewTraining
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new CVWithSelection();
+            MainPage = new CVWithRefresh();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
